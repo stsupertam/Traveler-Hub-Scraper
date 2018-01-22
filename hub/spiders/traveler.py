4561d6f71395e5cc214cc597315fba99a09f71ef
stsupertam <supmantam@gmail.com>
2018-01-23 02:12:09 +0700

    can crawl to another page

--- hub/spiders/traveler.py
+++ hub/spiders/traveler.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
+from urllib.parse import quote
 import scrapy
 
 
 class TravelerSpider(scrapy.Spider):
     name = 'traveler'
-    allowed_domains = ['https://www.noomsaotours.co.th/%E0%B8%97%E0%B8%B1%E0%B8%A7%E0%B8%A3%E0%B9%8C%E0%B9%83%E0%B8%99%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B9%80%E0%B8%97%E0%B8%A8/']
-    start_urls = ['http://https://www.noomsaotours.co.th/%E0%B8%97%E0%B8%B1%E0%B8%A7%E0%B8%A3%E0%B9%8C%E0%B9%83%E0%B8%99%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B9%80%E0%B8%97%E0%B8%A8//']
+    allowed_domains = ['noomsaotours.co.th']
+    start_urls = ['https://www.noomsaotours.co.th/%E0%B8%97%E0%B8%B1%E0%B8%A7%E0%B8%A3%E0%B9%8C%E0%B9%83%E0%B8%99%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B9%80%E0%B8%97%E0%B8%A8/']
+    package_url = []
+    counter = 0
 
     def parse(self, response):
-        pass
+        if not self.package_url:
+            self.package_url = response.xpath('//div[@class="tour_des_first1"]/div/a/@href').extract()
+        else:
+            self.counter += 1
+            print(self.counter)
+            print(response.xpath('//div[@class="eight columns  "]/div/h1/text()').extract_first())
+
+        for href in self.package_url:
+            yield response.follow(href, callback=self.parse)
