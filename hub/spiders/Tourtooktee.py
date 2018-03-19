51b557b75d116d1401e1c3314d2d6d92bd274d46
stsupertam <supmantam@gmail.com>
2018-03-20 00:46:21 +0700

    fix timeline day detail bug

--- hub/spiders/Tourtooktee.py
+++ hub/spiders/Tourtooktee.py
@@ -9,47 +9,38 @@ from hub.items import Package
 class TourtookteeSpider(scrapy.Spider):
     name = 'tourtooktee'
     allowed_domains = ['tourtooktee.com']
-    #start_urls = ['http://www.tourtooktee.com/programtour.php?cate=%E0%B8%A0%E0%B8%B2%E0%B8%A2%E0%B9%83%E0%B8%99%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B9%80%E0%B8%97%E0%B8%A8']
-    start_urls = ['http://www.tourtooktee.com/%E0%B8%97%E0%B8%B1%E0%B8%A7%E0%B8%A3%E0%B9%8C/incnx-02-%E0%B8%AD%E0%B8%B4%E0%B8%99%E0%B8%97%E0%B8%99%E0%B8%99%E0%B8%97%E0%B9%8C-%E0%B8%9B%E0%B8%B2%E0%B8%A2-%E0%B8%AD%E0%B8%A5%E0%B8%B1%E0%B8%87%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%93%E0%B9%8C%E0%B8%87%E0%B8%B2%E0%B8%99%E0%B8%A3%E0%B8%B2%E0%B8%8A%E0%B8%9E%E0%B8%A4%E0%B8%81%E0%B8%A9%E0%B9%8C']
+    start_urls = ['http://www.tourtooktee.com/programtour.php?cate=%E0%B8%A0%E0%B8%B2%E0%B8%A2%E0%B9%83%E0%B8%99%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B9%80%E0%B8%97%E0%B8%A8']
     package_urls = []
-    #start_urls = ['https://www.tourtooktee.co.th/%E0%B8%97%E0%B8%B1%E0%B8%A7%E0%B8%A3%E0%B9%8C%E0%B9%83%E0%B8%99%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B9%80%E0%B8%97%E0%B8%A8/%E0%B9%83%E0%B8%95%E0%B9%89/%E0%B9%80%E0%B8%95%E0%B9%87%E0%B8%A1%E0%B8%AD%E0%B8%B4%E0%B9%88%E0%B8%A1%E0%B8%81%E0%B8%B1%E0%B8%9A%E0%B8%A1%E0%B8%B1%E0%B8%A5%E0%B8%94%E0%B8%B5%E0%B8%9F%E0%B8%AA%E0%B9%8C%E0%B9%80%E0%B8%A1%E0%B8%B7%E0%B8%AD%E0%B8%87%E0%B9%84%E0%B8%97%E0%B8%A2-%E0%B8%95%E0%B8%B0%E0%B8%A3%E0%B8%B8%E0%B9%80%E0%B8%95%E0%B8%B2-%E0%B8%AB%E0%B8%A5%E0%B8%B5%E0%B9%80%E0%B8%9B%E0%B9%8A%E0%B8%B0-%E0%B8%AB%E0%B8%B2%E0%B8%94%E0%B9%83%E0%B8%AB%E0%B8%8D%E0%B9%88/207']
-    #package_urls = ['https://www.tourtooktee.co.th/%E0%B8%97%E0%B8%B1%E0%B8%A7%E0%B8%A3%E0%B9%8C%E0%B9%83%E0%B8%99%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B9%80%E0%B8%97%E0%B8%A8/%E0%B9%83%E0%B8%95%E0%B9%89/%E0%B9%80%E0%B8%95%E0%B9%87%E0%B8%A1%E0%B8%AD%E0%B8%B4%E0%B9%88%E0%B8%A1%E0%B8%81%E0%B8%B1%E0%B8%9A%E0%B8%A1%E0%B8%B1%E0%B8%A5%E0%B8%94%E0%B8%B5%E0%B8%9F%E0%B8%AA%E0%B9%8C%E0%B9%80%E0%B8%A1%E0%B8%B7%E0%B8%AD%E0%B8%87%E0%B9%84%E0%B8%97%E0%B8%A2-%E0%B8%95%E0%B8%B0%E0%B8%A3%E0%B8%B8%E0%B9%80%E0%B8%95%E0%B8%B2-%E0%B8%AB%E0%B8%A5%E0%B8%B5%E0%B9%80%E0%B8%9B%E0%B9%8A%E0%B8%B0-%E0%B8%AB%E0%B8%B2%E0%B8%94%E0%B9%83%E0%B8%AB%E0%B8%8D%E0%B9%88/207']
     page_urls = []
     page = 0
     total_package = 0
     total_page = 0
 
     def parse(self, response):
+        current_page = 'http://www.tourtooktee.com/programtour.php?pn=10&cate=ภายในประเทศ&page='
+        page = response.xpath('//*[@id="pagination"]/a[3]/@title').extract_first()
+        self.total_page = int(re.findall(r'\d', page)[0])
+        for i in range(self.total_page):
+            self.page_urls.append(current_page + str(i + 1))
+        for href in self.page_urls:
+            yield response.follow(href, callback=self.get_package_url)
+        print(self.page_urls)
+        
+    def get_package_url(self, response):
+        self.page += 1
+        package_urls = response.xpath('//div[@class="txt grid_7"]/h2/a/@href').extract()
+        for url in package_urls:
+            url = f'http://www.{self.name}.com{url[1:]}'
+            self.package_urls.append(url)
+        if(self.page == self.total_page):
+            for href in self.package_urls:
+                yield response.follow(href, callback=self.get_package)
+
+    def get_package(self, response):
+        self.total_package += 1
+        print(self.total_package)
         package = create_package(response)
         yield package
-
-    #def parse(self, response):
-    #    current_page = 'http://www.tourtooktee.com/programtour.php?pn=10&cate=ภายในประเทศ&page='
-    #    page = response.xpath('//*[@id="pagination"]/a[3]/@title').extract_first()
-    #    self.total_page = int(re.findall(r'\d', page)[0])
-    #    for i in range(self.total_page):
-    #        self.page_urls.append(current_page + str(i + 1))
-    #    for href in self.page_urls:
-    #        yield response.follow(href, callback=self.get_package_url)
-    #    print(self.page_urls)
-    #    
-    #def get_package_url(self, response):
-    #    self.page += 1
-    #    #packages_url = response.x[]
-    #    #package_urls = response.xpath('//*[@id="tour_list_wrapper"]/div[1]/div[2]/p/a/@href').extract()
-    #    #package_urls = response.xpath('//*[@id="tour_list_wrapper"]/div[@class="txt grid_7"]/h2/a/@href').extract()
-    #    package_urls = response.xpath('//div[@class="txt grid_7"]/h2/a/@href').extract()
-    #    for url in package_urls:
-    #        url = f'http://www.{self.name}.com{url[1:]}'
-    #        self.package_urls.append(url)
-    #    if(self.page == self.total_page):
-    #        for href in self.package_urls:
-    #            yield response.follow(href, callback=self.get_package)
-
-    #def get_package(self, response):
-    #    self.total_package += 1
-    #    package = create_package(response)
-    #    yield package
 
 def process_timeline(response):
     text = response.xpath('//div[@class="tab_container"]/div').extract()
@@ -57,7 +48,9 @@ def process_timeline(response):
     for day, div in enumerate(text):
         div = div.replace('\n', '').replace('\xa0', '')
         sel = Selector(text=div)
-        detail = sel.xpath('.//span/text()').extract()[0].strip()
+        detail = sel.xpath('.//span/text()').extract()
+        if(detail):
+            detail = detail[0].strip()
         temp = {}
         temp['day'] = day + 1
         temp['detail'] = detail
